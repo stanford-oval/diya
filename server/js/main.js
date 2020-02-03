7373b6097e919cfc1e50e0bce88bcab4ef0001c1
Michael Fischer <mfischer@cs.stanford.edu>
2020-02-03 08:55:53 -0800

    sycning up

--- server/js/main.js
+++ server/js/main.js
@@ -1,7 +1,6 @@
 "use strict";
 $(function() {
 
-
     // const selection = Selection.create({
 
     //     // Class for the selection-area
--- src/content-scripts/index.js
+++ src/content-scripts/index.js
@@ -3,6 +3,10 @@ import VoiceHandler from './VoiceHandler'
 import $ from "jquery";
 import Selection from "@simonwep/selection-js";
 
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+
+window.tippy = tippy
 window.$ = $
 window.Selection = Selection
 window.eventRecorder = new EventRecorder()
